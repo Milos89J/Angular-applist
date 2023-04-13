ab7c014e201011909d8b0d0767663f982347d9ec
Milos Jeremic <Milos.Jeremic@bdkadvokati.com>
2023-04-13 15:01:40 +0200

    first-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HerosComponent } from './components/heros/heros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule
